c744ba9cbdd28da20e67bd580efad1e7765befe6
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-24 21:46:18 +0900

    added: auth middleware

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,23 @@
 package auth
+
+import (
+	"github.com/hlts2/lilty"
+)
+
+// BasicAuth returns basic auth middleware for lilty framwwork
+func BasicAuth(cUsername, cPassword string) lilty.ChainHandler {
+	return func(handler lilty.Handler) lilty.Handler {
+		return func(ctxt *lilty.Context) {
+			username, password, ok := ctxt.Request.BasicAuth()
+
+			match := cUsername == username && cPassword == password
+
+			if !ok || !match {
+				// TODO send error
+				return
+			}
+
+			handler(ctxt)
+		}
+	}
+}
