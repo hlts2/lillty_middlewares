9909f1389f34691d6692cb29786388c082dddcb2
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-26 15:08:59 +0900

    added: config field of cors and custom type for allow-credentials header

--- cors/cors.go
+++ cors/cors.go
@@ -1,19 +1,46 @@
 package cors
 
 import (
+	"strings"
+	"time"
+
 	"github.com/hlts2/lilty"
 )
 
+// Credentials is custom type for Access-Control-Allow-Credentials
+type Credentials bool
+
+// String returns "true" if c is true, otherwise it returns "false"
+func (c Credentials) String() string {
+	switch c {
+	case true:
+		return "true"
+	case false:
+		return "false"
+	default:
+		return "false"
+	}
+}
+
 // Config represents configuration of cros
 type Config struct {
-	headers map[string]string
+	AllowOrigins     []string
+	AllowMethods     []string
+	AllowHeaders     []string
+	AllowCredentials Credentials
+	MaxAge           time.Duration
 }
 
 // New returns cors middleware for lilty framework
-func New() lilty.ChainHandler {
+func New(c Config) lilty.ChainHandler {
+	_ = strings.Join(c.AllowOrigins, ",")
+	_ = strings.Join(c.AllowMethods, ",")
+	_ = strings.Join(c.AllowHeaders, ",")
+	_ = c.AllowCredentials.String()
+
 	return func(next lilty.Handler) lilty.Handler {
 		return func(ctxt *lilty.Context) {
-			next(ctxt)
+			// TODO 処理を追加
 		}
 	}
 }
