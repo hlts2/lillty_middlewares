47bb0dffdcb3097cf048633847b54e659a1b701f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-08-30 15:11:16 +0900

    added: test case for basic_auth middleware

--- auth/basic/basic_auth_test.go
+++ auth/basic/basic_auth_test.go
@@ -1,1 +1,63 @@
 package auth
+
+import (
+	"encoding/base64"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/hlts2/lilty"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		config   Config
+		request  *http.Request
+		expected http.Header
+	}{
+		{
+			config: Config{
+				Username: "name",
+				Password: "pass",
+				Realm:    "Secret area",
+			},
+			request: &http.Request{
+				Header: http.Header{
+					"Authorization": {""},
+				},
+			},
+			expected: http.Header{
+				"Www-Authenticate": {`Basic realm="Secret area"`},
+			},
+		},
+		{
+			config: Config{
+				Username: "name",
+				Password: "pass",
+				Realm:    "Secret area",
+			},
+			request: &http.Request{
+				Header: http.Header{
+					"Authorization": []string{"Basic " + base64.StdEncoding.EncodeToString([]byte("name:pass"))},
+				},
+			},
+			expected: http.Header{},
+		},
+	}
+
+	for i, test := range tests {
+		writer := httptest.NewRecorder()
+		handler := New(test.config)(func(ctxt *lilty.Context) {})
+
+		handler(&lilty.Context{
+			Writer:  writer,
+			Request: test.request,
+		})
+
+		got := writer.Header()
+		if !reflect.DeepEqual(test.expected, got) {
+			t.Errorf("tests[%d] - New middleware is wrong. expected: %v, got: %v", i, test.expected, got)
+		}
+	}
+}
